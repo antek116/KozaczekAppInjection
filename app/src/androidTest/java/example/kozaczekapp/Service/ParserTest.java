8debfe39a06a8a1bc0d863c55f25cee40e7e6555
Krzysztof Antczak <antek116@gmail.com>
2016-03-31 13:16:48 -0700

    added tests

---
+++ app/src/androidTest/java/example/kozaczekapp/Service/ParserTest.java
@@ -0,0 +1,22 @@
+package example.kozaczekapp.Service;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+@RunWith(AndroidJUnit4.class)
+public class ParserTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
