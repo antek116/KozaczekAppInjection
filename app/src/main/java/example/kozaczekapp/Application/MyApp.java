7f31b133689d84167c2e42486a04d51aece48603
Krzysztof Antczak <antek116@gmail.com>
2016-04-06 13:15:38 -0700

    Adding test and troubleshooting code after test

--- app/src/main/java/example/kozaczekapp/Application/MyApp.java
+++ app/src/main/java/example/kozaczekapp/Application/MyApp.java
@@ -6,25 +6,17 @@ import example.kozaczekapp.Component.DaggerIConnectionComponent;
 import example.kozaczekapp.Component.IConnectionComponent;
 import example.kozaczekapp.Module.ConnectionModule;
 
+/**
+ * Creating SingleTone DaggerIConnectionComponent class
+ */
 public class MyApp extends Application {
 
     private static IConnectionComponent componentInstance = null;
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-
-        componentInstance = DaggerIConnectionComponent
-                .builder()
-                .connectionModule(new ConnectionModule(getBaseContext()))
-                .build();
-    }
-
     /**
      *
      * @return Singleton istance of IconnectionnCommponent.
      */
-    public  IConnectionComponent getComponentInstance() {
+    public IConnectionComponent getComponentInstance(){
         if (componentInstance == null) {
             return componentInstance = DaggerIConnectionComponent
                     .builder()
--- app/src/main/java/example/kozaczekapp/Service/KozaczekService.java
+++ app/src/main/java/example/kozaczekapp/Service/KozaczekService.java
@@ -9,12 +9,9 @@ import java.util.ArrayList;
 import javax.inject.Inject;
 
 import example.kozaczekapp.Application.MyApp;
-import example.kozaczekapp.Component.DaggerIConnectionComponent;
-import example.kozaczekapp.Component.IConnectionComponent;
 import example.kozaczekapp.ConnectionProvider.IConnection;
 import example.kozaczekapp.DatabaseConnection.DatabaseHandler;
 import example.kozaczekapp.KozaczekItems.Article;
-import example.kozaczekapp.Module.ConnectionModule;
 
 
 /**
@@ -24,8 +21,6 @@ public class KozaczekService extends IntentService {
 
     public static final String URL = "url";
     private static final String TAG = "KozaczekService";
-    private IConnectionComponent component;
-
     /**
      * Creates an IntentService.  Invoked by your subclass's constructor.
      */
