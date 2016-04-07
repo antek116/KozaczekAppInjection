3e860f69cd8053a7fedf9edd341850a1fdac843c
mkaczynska <magdalena.kaczynska@blstream.com>
2016-04-07 11:16:00 +0200

    Authenticator cleanup

--- app/src/main/java/example/kozaczekapp/authenticator/AccountAuthenticator.java
+++ app/src/main/java/example/kozaczekapp/authenticator/AccountAuthenticator.java
@@ -8,7 +8,6 @@ import android.accounts.NetworkErrorException;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 
 public class AccountAuthenticator extends AbstractAccountAuthenticator implements AccountConstants {
 
@@ -63,46 +62,7 @@ public class AccountAuthenticator extends AbstractAccountAuthenticator implement
      */
     @Override
     public Bundle getAuthToken(AccountAuthenticatorResponse response, Account account, String authTokenType, Bundle options) throws NetworkErrorException {
-
-        // If the caller requested an authToken type we don't support, then
-        // return an error
-        if (!authTokenType.equals(AccountConstants.AUTHTOKEN_TYPE_READ_ONLY) && !authTokenType.equals(AccountConstants.AUTHTOKEN_TYPE_FULL_ACCESS)) {
-            final Bundle result = new Bundle();
-            result.putString(AccountManager.KEY_ERROR_MESSAGE, "invalid authTokenType");
-            return result;
-        }
-
-        // Extract the username and password from the Account Manager, and ask
-        // the server for an appropriate AuthToken.
-        final AccountManager am = AccountManager.get(context);
-        String authToken = am.peekAuthToken(account, authTokenType);
-
-
-        // Lets give another try to authenticate the user
-        if (TextUtils.isEmpty(authToken)) {
-            authToken = "TEST_AUTH_TOKEN";
-        }
-
-        // If we get an authToken - we return it
-        if (!TextUtils.isEmpty(authToken)) {
-            final Bundle result = new Bundle();
-            result.putString(AccountManager.KEY_ACCOUNT_NAME, account.name);
-            result.putString(AccountManager.KEY_ACCOUNT_TYPE, account.type);
-            result.putString(AccountManager.KEY_AUTHTOKEN, authToken);
-            return result;
-        }
-
-        // If we get here, then we couldn't access the user's password - so we
-        // need to re-prompt them for their credentials. We do that by creating
-        // an intent to display our AuthenticatorActivity.
-        final Intent intent = new Intent(context, AuthenticatorActivity.class);
-        intent.putExtra(AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE, response);
-        intent.putExtra(AuthenticatorActivity.ARG_ACCOUNT_TYPE, account.type);
-        intent.putExtra(AuthenticatorActivity.ARG_AUTH_TYPE, authTokenType);
-        intent.putExtra(AuthenticatorActivity.ARG_ACCOUNT_NAME, account.name);
-        final Bundle bundle = new Bundle();
-        bundle.putParcelable(AccountManager.KEY_INTENT, intent);
-        return bundle;
+        return null;
     }
 
     /**
@@ -110,12 +70,7 @@ public class AccountAuthenticator extends AbstractAccountAuthenticator implement
      */
     @Override
     public String getAuthTokenLabel(String authTokenType) {
-        if (AccountConstants.AUTHTOKEN_TYPE_FULL_ACCESS.equals(authTokenType))
-            return AUTHTOKEN_TYPE_FULL_ACCESS_LABEL;
-        else if (AUTHTOKEN_TYPE_READ_ONLY.equals(authTokenType))
-            return AUTHTOKEN_TYPE_READ_ONLY_LABEL;
-        else
-            return authTokenType + " (Label)";
+        return null;
     }
 
     /**
@@ -131,8 +86,6 @@ public class AccountAuthenticator extends AbstractAccountAuthenticator implement
      */
     @Override
     public Bundle hasFeatures(AccountAuthenticatorResponse response, Account account, String[] features) throws NetworkErrorException {
-        final Bundle result = new Bundle();
-        result.putBoolean(AccountManager.KEY_BOOLEAN_RESULT, false);
-        return result;
+        return null;
     }
 }
