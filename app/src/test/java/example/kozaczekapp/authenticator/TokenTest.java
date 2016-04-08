ecce2dbd45fd4fc96113a741cd57516cdd6422b8
mkaczynska <magdalena.kaczynska@blstream.com>
2016-04-08 14:34:12 +0200

    Reformat

--- app/src/test/java/example/kozaczekapp/authenticator/TokenTest.java
+++ app/src/test/java/example/kozaczekapp/authenticator/TokenTest.java
@@ -1,10 +1,9 @@
 package example.kozaczekapp.authenticator;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class TokenTest {
 
